a9bacd1de20565b6c757255b9c1ca6126b2ea414
Steve Schaefer <stephen.schaefer@me.com>
2015-12-31 17:36:07 -0500

    Updated JAR file and mastermind.js.

--- javascripts/mastermind.js
+++ javascripts/mastermind.js
@@ -1,11 +1,11 @@
 function load_overview(){
-    document.getElementById("content").innerHTML='<object type="text/html" data="../Mastermind/html/overview.html" </object>';
+    document.getElementById("content").innerHTML='<object type="text/html" data="./html/overview.html" </object>';
 }
 
 function load_readme(){
-    document.getElementById("content").innerHTML='<object type="text/html" data="../Mastermind/html/readme.html" </object>';
+    document.getElementById("content").innerHTML='<object type="text/html" data="./html/readme.html" </object>';
 }
 
 function load_javadocs(){
-    document.getElementById("content").innerHTML='<object type="text/html" data="../Mastermind/javadoc/index.html" </object>';
+    document.getElementById("content").innerHTML='<object type="text/html" data="./javadoc/index.html" </object>';
 }
